697f4a1539b61626b48de04dda5c34ad4d4e1b54
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-06-21 17:41:44 +0700

    Inited AS project

---
+++ app/src/main/java/dev/supasintatiyanupanwong/apps/android/bpi/MainActivity.kt
@@ -0,0 +1,14 @@
+package dev.supasintatiyanupanwong.apps.android.bpi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+}
