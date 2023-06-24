6521a57e8bbc43ffce22faf1f405376c71bd8213
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2023-06-24 20:58:56 +0700

    Scaffolded basic UI for testing

--- app/src/main/java/dev/supasintatiyanupanwong/apps/android/bpi/MainActivity.kt
+++ app/src/main/java/dev/supasintatiyanupanwong/apps/android/bpi/MainActivity.kt
@@ -3,25 +3,47 @@ package dev.supasintatiyanupanwong.apps.android.bpi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import dev.supasintatiyanupanwong.apps.android.bpi.currentprice.data.CurrentPriceRepository
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
     private val currentPriceRepository: CurrentPriceRepository by inject()
+
+    private val format = DecimalFormat()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        currentPriceRepository.observeCurrentPrice()
+            .flowWithLifecycle(lifecycle)
+            .onEach { record ->
+                val firstPrice = record?.prices?.getOrNull(0)
+                findViewById<TextView>(R.id.conversion_type).text =
+                    "BTC/${firstPrice?.currency?.currencyCode}"
+                findViewById<TextView>(R.id.price).text =
+                    firstPrice?.let { "${it.currency.symbol} ${format.format(it.value)}" }
+            }
+            .launchIn(lifecycleScope)
+
+        findViewById<SwipeRefreshLayout>(R.id.refresh_layout).setOnRefreshListener { invalidate() }
+
+        invalidate()
+    }
+
+    private fun invalidate() {
         lifecycleScope.launch {
             currentPriceRepository.fetchCurrentPrice()
-
-            findViewById<TextView>(R.id.text)
-                .text = currentPriceRepository.getCurrentPrice().toString()
+            findViewById<SwipeRefreshLayout>(R.id.refresh_layout).isRefreshing = false
         }
     }
 
